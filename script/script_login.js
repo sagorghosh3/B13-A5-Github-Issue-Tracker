846fd1f954415f386797f1cce1c80353011b7551
sagorghosh3 <ghoshsagor345@gmail.com>
2026-03-07 12:29:52 +0600

    sing-in function successfully okay

---
+++ script/script_login.js
@@ -0,0 +1,22 @@
+//SIGN-IN FUNCTION 
+document.getElementById("Signin-btn").addEventListener("click", function () {
+    // console.log("Signin successfully")
+    //1- get the user input
+    const userName = document.getElementById("input-Username");
+    // console.log(userName);
+    const User = userName.value;
+    console.log(User);
+
+    // 2- get the pass input
+    const inputPass = document.getElementById("input-password");
+    const password = inputPass.value;
+    console.log(password);
+    // 3-match pass & admin 
+    if (User == "admin" && password == "1234") {
+        alert("log in successfully");
+        window.location.assign("/home.html");
+    } else {
+        alert("login Failed");
+        return;
+    }
+})
